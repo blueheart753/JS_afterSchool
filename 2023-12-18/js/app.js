159eca54a8798b5cd4a8b428c8ccb3b35a042901
kimminam <bluemind8520@gmail.com>
2024-01-01 21:02:25 +0900

    2024-01-01

--- 2023-12-18/js/app.js
+++ 2023-12-18/js/app.js
@@ -1,66 +1,73 @@
-const navBar = document.querySelectorAll(".nav_bar > li");
-const navMenu = document.querySelector(".menu");
-const navMenuList = document.querySelectorAll(".menu > ul > li");
+// window.addEventListener('load', () => {
+//   setTimeout(() => window.scrollTo(0, 0), 100);
+// });
 
-navBar.forEach((el, idx) => {
-  el.addEventListener("mouseover", () => {
-    navMenu.classList.add("active");
+const navBar = document.querySelectorAll('.nav_bar > li');
+const navMenu = document.querySelector('.menu');
+const navMenuList = document.querySelectorAll('.menu > ul > li');
 
-    if (navMenuList[idx]) {
-      navMenuList[idx].classList.add("hovered");
-    }
+const content = document.querySelectorAll('.content-wrap > ul > li');
+const sectionPage = document.querySelectorAll('section');
+let sectionPosition;
+
+const moveSection = idx => {
+  sectionPosition = sectionPage[idx].offsetTop;
+  window.scrollTo({ top: sectionPosition, behavior: 'smooth' });
+};
+
+const clickMoveSectionEvent = (el, idx) => {
+  el.addEventListener('click', e => {
+    e.preventDefault();
+    moveSection(idx);
   });
-
-  el.addEventListener("mouseout", () => {
-    navMenu.classList.remove("active");
-    if (navMenuList[idx]) {
-      navMenuList[idx].classList.remove("hovered");
-    }
-  });
-});
-
-navMenu.addEventListener("mouseover", () => {
-  navMenu.classList.add("active");
-});
-
-navMenu.addEventListener("mouseout", () => {
-  navMenu.classList.remove("active");
-});
-
-const content = document.querySelectorAll(".content-wrap > ul > li");
+};
 
 content.forEach((el, idx) => {
-  el.addEventListener("click", (e) => {
-    e.preventDefault();
-    switch (idx) {
-      case 0:
-        break;
+  clickMoveSectionEvent(el, idx);
+});
 
-      case 1:
-        location.href = "./view/About.html";
-        break;
+navBar.forEach((el, idx) => {
+  el.addEventListener('mouseover', () => {
+    navMenu.classList.add('active');
 
-      case 2:
-        location.href = "./view/Skills.html";
-        break;
-
-      case 3:
-        location.href = "./view/Portfolio.html";
-        break;
-
-      default:
-        location.href = "./view/Portfolio.html";
-        break;
+    if (navMenuList[idx]) {
+      navMenuList[idx].classList.add('hovered');
     }
+  });
+
+  el.addEventListener('mouseout', () => {
+    navMenu.classList.remove('active');
+    if (navMenuList[idx]) {
+      navMenuList[idx].classList.remove('hovered');
+    }
+  });
+  clickMoveSectionEvent(el, idx);
+});
+
+navMenu.addEventListener('mouseover', () => {
+  navMenu.classList.add('active');
+});
+
+navMenu.addEventListener('mouseout', () => {
+  navMenu.classList.remove('active');
+});
+
+navMenuList.forEach((el, idx) => {
+  clickMoveSectionEvent(el, idx);
+});
+
+const subTitle = document.querySelector('.home > .showInterduce > h2');
+const headerContent = document.querySelector('.home > .showInterduce>p');
+const headerImage = document.querySelector('.photo_wrap');
+
+const io = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    entry.isIntersecting
+      ? entry.target.classList.add('show')
+      : entry.target.classList.remove('show');
   });
 });
 
-const subTitle = document.querySelector(".home > .showInterduce > h2");
-const headerContent = document.querySelector(".home > .showInterduce>p");
-const headerImage = document.querySelector(".photo_wrap");
-
-window.addEventListener("load", () => {
-  subTitle.classList.add("active");
-  headerContent.classList.add("startDoc");
-  headerImage.classList.add("startDoc");
+document.querySelectorAll('.animate').forEach(el => {
+  io.observe(el);
 });
